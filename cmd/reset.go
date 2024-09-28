7f8e32710873b986a6443985aeaa19cf43ff47b1
Terry Arthur Brooks, Jr. <Terry.Arthur@BrooksJr.com>
2024-09-28 15:30:14 -0400

    Init Commit

---
+++ cmd/reset.go
@@ -0,0 +1,34 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// resetCmd represents the reset command
+var resetCmd = &cobra.Command{
+	Use:   "reset",
+	Short: "Erase all Past SetUps",
+	Long:  `"This command will reverse all of the previously completed Server Configuration Tasks"`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("reset called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(resetCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// resetCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// resetCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2024 Terry Brooks Terry@BrooksJr.com
+
+*/
+package main
+
+import "github.com/terry-brooksjr/TheConfigurator/cmd"
+
+func main() {
+	cmd.Execute()
+}
